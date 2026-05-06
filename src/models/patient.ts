c71e7ceab7739e77520dc8bbab82f82b619c49ba
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-05-06 10:48:08 +0000

    Modelo de paciente terminado y staff a la mitad

---
+++ src/models/patient.ts
@@ -0,0 +1,86 @@
+import { Document, model, Schema } from "mongoose";
+import validator from "validator";
+
+/**
+ * Interfaz que define la forma del documento del paciente
+ */
+interface PatientDocumentationInterface extends Document {
+    fullName: string;
+    birthDate: Date;
+    idNumber: string;
+    socialSecurityNumber: string;
+    gender: string;
+    contactData: {
+        address: string;
+        phoneNumber: string;
+        email: string;
+    };
+    allergies: string[];
+    bloodType: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "0+" | "0-";
+    status: "activo" | "baja temporal" | "fallecido";
+}
+
+const PatientSchema = new Schema<PatientDocumentationInterface>({
+    fullName: {
+        type: String,
+        required: [true, "El nombre completo es obligatorio"],
+        trim: true
+    },
+    birthDate: {
+        type: Date,
+        required: [true, "La fecha de nacimiento es obligatoria"],
+    },
+    idNumber: {
+        type: String,
+        required: [true, "El número de identificación es obligatorio"],
+        unique: true,
+        trim: true
+    },
+    socialSecurityNumber: {
+        type: String,
+        required: [true, "El número de la seguridad social es obligatorio"],
+        unique: true,
+        trim: true
+    },
+    gender: {
+        type: String,
+        required: [true, "El género del paciente es obligatorio"],
+        trim: true
+    },
+    contactData: {
+        address: { type: String, required: [true, "La dirección es obñigatoria"], trim: true },
+        phoheNumber: { type: String, required: [true, "El número de teléfono es obligatorio"], trim: true },
+        email: {
+            type: String,
+            required: [true, "El email es obligatorio"],
+            unique: true,
+            trim: true,
+            lowercase: true,
+            validate(value: string) {
+                if (!validator.default.isEmail(value)) {
+                    throw new Error("El email del paciente no es válido");
+                }
+            }
+        }
+    },
+    allergies: {
+        type: [String],
+        default: []
+    },
+    bloodType: {
+        type: String,
+        required: [true, "El tipo de sangre es obligatorio"],
+        enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "0+", "0-"]
+    },
+    status: {
+        type: String,
+        required: [true, "El estado del paciente es obligatorio"],
+        default: "activo",
+        enum: ["activo", "baja temporal", "fallecido"]
+    },
+});
+
+/**
+ * Se invoca a model que tiene como argumentos el nombre del modelo y el esquema del paciente.
+ */
+export const Patient = model<PatientDocumentationInterface>("Patient", PatientSchema);
---
+++ src/models/staff.ts
@@ -0,0 +1,53 @@
+import { Document, model, Schema } from "mongoose";
+import validator from "validator";
+
+/**
+ * Interfaz que define la forma del documento del personal médico
+ */
+interface StaffDocumentationInterface extends Document {
+    fullName: string;
+    licenseNumber: string;
+    specialty: "medicina general" | "cardiología" | "traumatología" | "pediatría" | "oncología" | "urgencias";
+    role: "médico/a adjunto/a" | "médico/a residente" | "enfermero/a" | "auxiliar de enfermería" | "jefe/a de servicio";
+    shift: "mañana" | "tarde" | "noche" | "rotatorio";
+    assignedArea: string;
+    yearsOfExperience: number;
+    contactData: {
+        address: string;
+        phoneNumber: string;
+        email: string;
+    }
+    status: "activo" | "inactivo";
+}
+
+const StaffSchema = new Schema<StaffDocumentationInterface>({
+    fullName: {
+        type: String,
+        required: [true, "El nombre completo es obligatorio"],
+        trim: true
+    },
+    licenseNumber: {
+        type: String,
+        required: [true, "El número de colegiado es obligatorio"],
+        unique: true,
+        trim: true
+    },
+    specialty: {
+        type: String,
+        required: [true, "La especielidad médica es obligatoria"],
+        enum: ["medicina general", "cardiología", "traumatología", "pediatría", "oncología", "urgencias"]
+    },
+    role: {
+        type: String,
+        required: [true, "La categoría profecional es obligatoria"],
+        enum: ["médico/a adjunto/a", "médico/a residente", "enfermero/a", "auxiliar de enfermería", "jefe/a de servicio"]
+    },
+    shift: {
+        //Completar
+    },
+});
+
+/**
+ * Se invoca a model que tiene como argumentos el nombre del modelo y el esquema del personal médico.
+ */
+export const Staff = model<StaffDocumentationInterface>("Staff", StaffSchema);
